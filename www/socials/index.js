17756cea338c8137003cae9aa4e9cb258fd0d560
Luna Nordbergh <dev@lunalu.org>
2024-10-18 02:13:18 +0200

    Added Responsive Layout

--- www/socials/index.js
+++ www/socials/index.js
@@ -1,6 +1,7 @@
 export function openLink(index){
   
   const link_address = [
+    'https://only-fans.uk/LunaLu',
     'https://discord.com/users/1098996480011944017', 
     'https://www.twitch.tv/lunaluplayz', 
     'https://vrchat.com/home/user/usr_19d6e456-23a6-41d4-8d0a-cf0ddd305e47', 
@@ -13,7 +14,6 @@ export function openLink(index){
     'https://wishlist.lunalu.org/user/4OKjWZpFk3Qt4pvsSAjS687svqj2',
     'https://steamcommunity.com/id/_LunaLu_/',
     'https://open.spotify.com/user/31jo3k6nxhloleusg3tidy7ctimq?si=93424dcbf62e47ea',
-    'https://only-fans.uk/LunaLu',
     'https://myanimelist.net/profile/LunaLuPlayz'
   ];
   
