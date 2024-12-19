39d83c42b76a4ca8b78128cedd2eb97c106a06d1
Luna Nordbergh <dev@lunalu.org>
2024-12-19 13:15:24 +0100

    Update HTML structure, styling, and language settings
    Changed language attribute from NL to EN across pages and adjusted titles for consistency. Improved HTML formatting, added hover animations to link boxes, and refined CSS for better UI. Minor updates to links and alt attributes for images.

--- www/socials/index.js
+++ www/socials/index.js
@@ -1,10 +1,10 @@
 export function openLink(index){
-
+  
   const link_address = [
     'https://only-fans.uk/LunaLu',
-    'https://discord.com/users/1098996480011944017',
-    'https://www.twitch.tv/lunaluplayz',
-    'https://vrchat.com/home/user/usr_19d6e456-23a6-41d4-8d0a-cf0ddd305e47',
+    'https://discord.com/users/1098996480011944017', 
+    'https://www.twitch.tv/lunaluplayz', 
+    'https://vrchat.com/home/user/usr_19d6e456-23a6-41d4-8d0a-cf0ddd305e47', 
     'https://www.reddit.com/user/LunaLu_04',
     'https://github.com/LunaLu-dev',
     'https://www.youtube.com/@Luna_Lu1',
@@ -16,6 +16,6 @@ export function openLink(index){
     'https://open.spotify.com/user/31jo3k6nxhloleusg3tidy7ctimq?si=93424dcbf62e47ea',
     'https://myanimelist.net/profile/LunaLuPlayz'
   ];
-
+  
   window.open(link_address[index]);
 }
