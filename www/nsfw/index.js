f5fc78c4591aa2933017394e64c23a4e6a74d435
Luna Nordbergh <dev@lunalu.org>
2025-03-12 01:30:24 +0100

    BDSM

---
+++ www/nsfw/index.js
@@ -0,0 +1,9 @@
+export function openLink(index){
+  
+  const link_address = [
+    'https://only-fans.uk/LunaLu',
+    'https://www.bdsmtest.org/r/jgeqMMPW',
+  ];
+  
+  window.open(link_address[index]);
+}
