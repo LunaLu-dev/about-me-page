b060196db14c34a0c60f4d6581fcabf5e1139b35
Luna Nordbergh <dev@lunalu.org>
2025-04-01 19:00:21 +0200

    Update the NSFW wishlist link to include a category query parameter
    Modified the link structure for the NSFW wishlist to ensure it includes the `category=nsfw` query parameter. This ensures proper categorization and improved functionality of the link.

--- www/nsfw/index.js
+++ www/nsfw/index.js
@@ -2,7 +2,7 @@ export function openLink(index){
   
   const link_address = [
     'https://www.bdsmtest.org/r/jgeqMMPW',
-    'https://wishlist.lunalu.org/nsfw'
+    'https://wishlist.lunalu.org?category=nsfw'
   ];
   
   window.open(link_address[index]);
