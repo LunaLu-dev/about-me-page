a67fea86d73d90d07a62da174880e05192c5ae17
Luna Nordbergh <dev@lunalu.org>
2025-04-05 00:19:31 +0200

    Update NSFW wishlist URL in index.js
    Modified the URL structure to use a query parameter for category. This ensures consistency with the updated endpoint format.

--- www/nsfw/index.js
+++ www/nsfw/index.js
@@ -2,7 +2,7 @@ export function openLink(index){
   
   const link_address = [
     'https://www.bdsmtest.org/r/jgeqMMPW',
-    'https://wishlist.lunalu.org?category=nsfw'
+    'https://wishlist.lunalu.org/category?category=nsfw'
   ];
   
   window.open(link_address[index]);
